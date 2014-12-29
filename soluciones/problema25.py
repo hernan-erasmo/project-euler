2e5763096d5b5972c3b6fd65278f9be967e0269f
Hernán Erasmo <socialmetrix@gmail.com>
2014-12-29 12:03:14 -0300

    Que vergüenza, calcular la longitud de los strings :'(

--- soluciones/problema25.py
+++ soluciones/problema25.py
@@ -3,18 +3,18 @@ fib_inicial = [1,1]		#Primeros dos numeros de la secuencia de Fibonacci
 def main():
 	lista = fib_inicial
 	indice = 1
+	tope = 10**999
 
 	for n in itertools.count(start=1,step=1):
-		lista.append(lista[len(lista)-1]+lista[len(lista)-2])
+		lista.append(lista[0]+lista[1])
 		del(lista[0])
 		indice += 1
 
-		if (len(str(lista[0])) or len(str(lista[1]))) >= 1000:
+		if (lista[0] >= tope) or (lista[1] >= tope):
 			break
 
-	print "La lista contiene los elementos {0} y {1} de la secuencia.".format(indice, indice + 1)
+	print "La lista contiene los elementos {0}({1} caracteres) y {2}({3} caracteres) de la secuencia.".format(indice, len(str(lista[0])), indice + 1,len(str(lista[1])))
 	print lista
-	print "Los numeros tienen una longitud de {0} y {1} caracteres.".format(len(str(lista[0])),len(str(lista[1])))
 
 if __name__ == '__main__':
 	import itertools
