5fdabd3943e9893ebecf6e9a12b6680a9db8b9bc
Hernán Erasmo <socialmetrix@gmail.com>
2014-12-29 14:34:41 -0300

    Emprolijando

--- soluciones/problema25.py
+++ soluciones/problema25.py
@@ -1,20 +1,16 @@
-fib_inicial = [1,1]		#Primeros dos numeros de la secuencia de Fibonacci
-
 def main():
+	fib_inicial = [1,1]		#Primeros dos numeros de la secuencia de Fibonacci
 	lista = fib_inicial
-	indice = 1
 	tope = 10**999
 
 	for n in itertools.count(start=1,step=1):
 		lista.append(lista[0]+lista[1])
 		del(lista[0])
-		indice += 1
 
 		if (lista[0] >= tope) or (lista[1] >= tope):
 			break
 
-	print "La lista contiene los elementos {0}({1} caracteres) y {2}({3} caracteres) de la secuencia.".format(indice, len(str(lista[0])), indice + 1,len(str(lista[1])))
-	print lista
+	print "La respuesta es", n + len(fib_inicial)
 
 if __name__ == '__main__':
 	import itertools
