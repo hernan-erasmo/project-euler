9332c6c20c9661fa98e06157b701556402dd4be4
Hernán Erasmo <socialmetrix@gmail.com>
2015-01-02 10:19:05 -0300

    Respuesta al problema 18

--- soluciones/problema18.py
+++ soluciones/problema18.py
@@ -14,41 +14,41 @@ piramide = """75
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 
-def suma_piramide(piramide):
-	lista_plana = [item for sublist in piramide for item in sublist]	#http://stackoverflow.com/a/952952/1603080
-	return sum([int(x) for x in lista_plana])	
+piramide_base = [x.split(' ') for x in piramide.split('\n')]
+
+def get_secuencia(camino):
+	"""Crea la secuencia de numeros que corresponden al camino indicado, incluyendo la raiz,
+	y los convierte a numeros.
+	0 = el camino va hacia la izquierda
+	1 = el camino va hacia la derecha"""
+	
+	indice_anterior = 0
+	resultado = [piramide_base[0][0]]
+
+	for nivel,direccion in enumerate(camino, start=1):
+		if direccion == '0':
+			puntero = indice_anterior
+		else:
+			puntero = indice_anterior + 1
+
+		indice_anterior = puntero
+		resultado.append(piramide_base[nivel][puntero])
+
+	return [int(x) for x in resultado]
 
 def main():
-	"""El algoritmo funciona de la siguiente manera.
-		(asumimos que la piramide tiene al menos un nivel, el cero, la raiz)
-		0- Agregamos la raiz de la piramide al camino solucion.
-		1- Si la piramide no tiene nivel 1, mostramos el camino solucion y salimos.
-		2- Generamos dos piramides nuevas, cuyas raices son los numeros del nivel 1.
-		3- Por cada piramide, calculamos la suma de todos los numeros que la componen.
-		4- Tomamos como nueva piramide a aquella que tenga la suma mas grande.
-			4.1- Si la suma es igual, tomamos la derecha, que es el camino de Dios.
-		5- Volvemos al paso 0"""
+	camino_max = ''
+	suma_max = 0
 
-	piramide_base = [x.split(' ') for x in piramide.split('\n')]
-	camino_solucion = [piramide_base.pop(0)[0]]
-	
-	while len(piramide_base) > 1:
-		piramide_izq = [piramide_base[i][:i+1] for i in range(len(piramide_base))]
-		piramide_der = [piramide_base[i][-i-1:] for i in range(len(piramide_base))]
+	for x in xrange(2**14):
+		secuencia = get_secuencia(bin(x)[2:].zfill(14))
+		suma = sum(secuencia)
 
-		if suma_piramide(piramide_izq) > suma_piramide(piramide_der):
-			piramide_base = piramide_izq
-		else:
-			piramide_base = piramide_der
+		if suma > suma_max:
+			suma_max = suma
+			camino_max = secuencia
 
-		camino_solucion.append(piramide_base.pop(0)[0])
-
-	#Agregamos el elemento mas grande de la lista de 2 que nos queda al final
-	ultimo_nivel = piramide_base.pop()
-	camino_solucion.append(ultimo_nivel[0] if ultimo_nivel[0] > ultimo_nivel[1] else ultimo_nivel[1])
-
-	print camino_solucion
-	print "La suma es:", sum([int(x) for x in camino_solucion])
+	print camino_max, suma_max
 
 if __name__ == '__main__':
 	main()
